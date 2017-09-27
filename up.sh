2c81703367678456a82f7b96cf2ba42e85a2f811
Eduardo Parra <edue17@gmail.com>
2017-09-27 19:30:08 +0200

    Update script

--- up.sh
+++ up.sh
@@ -23,8 +23,47 @@ for i in $DIRS
       git commit -m "$MEN"
       git push
       
-      ID=$(git rev-parse HEAD)
-      echo $ID
+      sleep 15
+      
+      DATA=`curl -s 'https://api.travis-ci.org/soker90/aur-betcon.svg?branch=master'`
+      PASS=0
+
+      if [ "`echo "$DATA" | grep passing`" != '' ]
+      then
+        PASS=1
+      fi
+      
+      if [ "`echo "$DATA" | grep fail`" != '' ]
+      then
+        PASS=1
+      fi
+      
+      echo $PASS
+
+      while [ $PASS == 0 ]
+      do
+        sleep 120
+        DATA=`curl -s 'https://api.travis-ci.org/soker90/aur-betcon.svg?branch=master'`
+        if [ $DATA | grep passing == "passing" ]
+        then
+          PASS=1
+          COUNT=${#DIR}
+          NAME=`cat $i | sed "s/$DIR//"`
+          mkdir ../../$NAME
+          cd ../../$NAME
+          PACKAGE=`echo $NAME | sed "s/$DIR//"`
+          git ssh://aur@aur.archlinux.org/$PACKAGE.git
+          cp $i/* .
+          rm .git .travis.yml LICENSE README.md
+        fi
+        
+        if [ $DATA | grep passing == "fail" ]
+        then
+          PASS=1
+          echo "ERROR en el paquete " + $PACKAGE
+        fi
+          
+      done
     fi
   done
 
